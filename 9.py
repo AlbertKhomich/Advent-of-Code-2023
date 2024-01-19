85c36288dcd95eabd1580ce1fb35f8511e48f054
Albert Khomich <khomich1022@gmail.com>
2024-01-19 17:03:45 +0100

    Days 8, 9

---
+++ 9.py
@@ -0,0 +1,28 @@
+from main import lines
+
+
+result = 0
+
+
+def organize_input(array):
+    return list(map(lambda x: list(map(lambda y: int(y), x.split())), array))
+
+
+def solve_p1(array):
+    new_array = [array[n+1] - array[n] for n in range(len(array)-1)]
+    if all(x == 0 for x in new_array):
+        return array[-1]
+    return array[-1] + solve_p1(new_array)
+
+
+for line in organize_input(lines):
+    result += solve_p1(line)
+print(result)
+# 1974913369
+
+
+
+
+
+
+
